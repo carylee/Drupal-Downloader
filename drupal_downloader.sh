5592333ad6dcf7c4a43028c55687b52bb4170359
Cary <cary@dhcp-129-105-110-210.at.northwestern.edu>
2009-10-08 15:29:11 -0500

    Added some comments and additional text output

--- drupal_downloader.sh
+++ drupal_downloader.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if which drush; then
+if which drush >/dev/null; then
   DRUSH="drush"
+  echo "Drush installation detected"
 else
+  echo "No Drush installation detected... installing drush temporarily"
   curl http://ftp.drupal.org/files/projects/drush-All-Versions-2.0.tar.gz > drush.tar.gz
   tar xvzf drush.tar.gz
   rm drush.tar.gz
@@ -10,29 +12,34 @@ else
   DRUSH=$PWD"/drush/drush"
 fi
 
+#Save the directory the script is running from
 script_dir=`dirname "$0"`
 
+#Check if drupal is installed in current folder
 DRUPAL=$(ls | grep -i drupal)
-if [ ! $DRUPAL ]
-then 
+if [ ! $DRUPAL ]; then 
   $DRUSH dl drupal
   DRUPAL=$(ls | grep -i drupal)
 fi
 
+#Look for a local modules.txt file
 if [ -f $script_dir/modules.txt ]
 then
+  echo "Local module list found at $script_dir/modules.txt"
   MODULES=$( cat $script_dir/modules.txt )
 else
-  MODULES=$( curl http://condo.mine.nu/~Cary/urls.txt )
+  echo "No local module list found... fetching a remote one"
+  MODULES=$( curl http://condo.mine.nu/~Cary/modules.txt )
 fi
 
 cd $DRUPAL/modules
 
+echo "Installing modules..."
 $DRUSH dl $MODULES
 #$DRUSH enable $MODULES 
 
 #This line is optional - it just removes the drush install when done
-if which drush; then
+if which drush >/dev/null; then
   echo done  
 else
   rm -R $TOP/drush
